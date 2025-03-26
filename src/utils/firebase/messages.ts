fc1b18d12ba6bff2b46888a9fdbb728ccbcb0500
Anthony <anthonyrov@gmail.com>
2025-03-26 12:48:47 +0100

    refactor: simplify message creation by removing server timestamp from Firestore document

--- src/utils/firebase/messages.ts
+++ src/utils/firebase/messages.ts
@@ -1,4 +1,4 @@
-import { collection, addDoc, serverTimestamp } from "firebase/firestore";
+import { collection, addDoc } from "firebase/firestore";
 import { ContactForm } from "./data-contracts";
 import { db } from "./firebase";
 import { checkRateLimit } from "../security";
@@ -6,10 +6,7 @@ import { checkRateLimit } from "../security";
 export const createMessage = async (form: ContactForm) => {
   if (!checkRateLimit()) throw new Error("Too many requests");
   try {
-    const docRef = await addDoc(collection(db, "messages"), {
-      ...form,
-      createdAt: serverTimestamp(),
-    });
+    const docRef = await addDoc(collection(db, "messages"), form);
     console.log("Message written with id: ", docRef.id);
   } catch (error) {
     console.error("Error adding message: ", error);
